0747f84705e939f6556a5d26fe38aae23296e54d
setu <setu@orangetoolz.com>
2024-01-07 12:20:49 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Builder from "./components/builder/Builder";
+
+function App() {
+  return (
+    <div className="App">
+      <Builder/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@mui/material";
+
+const Navbar:React.FC = () => {
+    return (
+        <Box p={2} height={60} bgcolor={"navy"} sx={{width:'100%'}}>
+
+        </Box>
+    );
+};
+
+export default Navbar;
